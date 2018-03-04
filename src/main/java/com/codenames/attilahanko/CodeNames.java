1f9f0399379787f973cdc25f14c7f609c0e98720
hankoattila <hattilla4@gmail.com>
2018-03-04 14:09:59 +0100

    Initial commit

---
+++ src/main/java/com/codenames/attilahanko/CodeNames.java
@@ -0,0 +1,13 @@
+package com.codenames.attilahanko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNames {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeNames.class, args);
+    }
+
+}
