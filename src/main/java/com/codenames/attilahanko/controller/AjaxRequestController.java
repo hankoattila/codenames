ef0e709b409f01714ab5d956c1e731c7a2ef6b8d
hankoattila <hattilla4@gmail.com>
2018-03-08 18:36:26 +0100

    Front end refactor

--- src/main/java/com/codenames/attilahanko/controller/AjaxRequestController.java
+++ src/main/java/com/codenames/attilahanko/controller/AjaxRequestController.java
@@ -2,19 +2,12 @@ package com.codenames.attilahanko.controller;
 
 import com.codenames.attilahanko.model.game.Card;
 import com.codenames.attilahanko.model.game.Game;
-import com.codenames.attilahanko.model.game.Team;
-import com.codenames.attilahanko.model.player.Player;
-import com.codenames.attilahanko.model.player.PlayerDTO;
-import com.codenames.attilahanko.model.player.User;
 import com.codenames.attilahanko.service.GameService;
-import com.codenames.attilahanko.service.implementation.game.InGameService;
-import com.codenames.attilahanko.service.implementation.GameServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,11 +15,11 @@ public class InGameController {
 
 
     private GameService gameService;
-    private InGameService inGameService;
 
-    public InGameController(GameService gameService, InGameService inGameService) {
+
+    public InGameController(GameService gameService) {
         this.gameService = gameService;
-        this.inGameService = inGameService;
+
     }
 
     @PostMapping("/bossEdit")
